a9d6eccc43b9565ad922a05488823c7fa9f35f37
Markus Mulkahainen <markus.mulkahainen@hotmail.com>
2016-06-13 19:32:30 +0300

    removes form navigation

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,9 +1,7 @@
 var enableInfoDisplay = require("./display_info.js");
 var enableDialogs = require("./dialogs.js");
-var enableFormNavigation = require("./formnavigation.js");
 var enableClickHandlers = require("./click_handlers.js");
 
 enableInfoDisplay();
 enableDialogs();
-enableFormNavigation();
 enableClickHandlers();
